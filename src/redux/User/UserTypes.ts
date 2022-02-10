6777c0e58e31343d60172fd52c835aef1cfd0ef3
quangvinht <96629186+quangvinht@users.noreply.github.com>
2022-02-10 14:08:35 +0700

    delete 10/2/2022

--- src/redux/User/UserTypes.ts
+++
@@ -1,3 +0,0 @@
-export const REQ = 'REQ'
-export const SUCCESS = 'SUCCESS'
-export const ERROR = 'ERROR'
--- src/redux/rootReducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import UserReducer from './User/UserReducer';
-
-const rootReducer = combineReducers({
-    user:UserReducer
-})
-
-export default rootReducer
